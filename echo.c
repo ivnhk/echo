c196db615d585a7fe9dcb9dbf20992865ef3e454
Ivan Hrekov <iandrc9@gmail.com>
2025-06-17 21:53:17 +0200

    implement similarly to shell

--- echo.c
+++ echo.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("argc = %d\n", argc);
-
-  for (int i = 0; i < argc; i++) {
-    printf("argv[%d] = %s\n", i, argv[i]);
+  for (int i = 1; i < argc; i++) {
+    printf("%s ", argv[i]);
   }
+  printf("\n");
 
   return 0;
 }
