c9fe7f2290aa91d9027046d1cf1055fccfa808ce
Ivan Hrekov <iandrc9@gmail.com>
2025-06-17 22:30:33 +0200

    add notes for future improvements

--- echo.c
+++ echo.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+// TODO: add documentation
 int main(int argc, char *argv[]) {
   int no_newline = argc > 1 && strcmp(argv[1], "-n") == 0;
   int i = no_newline ? 2 : 1;
@@ -16,14 +17,18 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+// TODO: re-write using pointers
 int strcmp(const char *s1, const char *s2) {
-  int i = 0;
+  int i = 0; // TODO: is this ok?
 
+  // NOTE: (unsigned char) cast is used to guarantee consistent comparison
+  // accross platforms. Otherwise, may give wrong results with extended ASCII or
+  // non-ASCII chars
   while (s1[i] != '\0' && s2[i] != '\0') {
-    if (s1[1] != s2[2]) {
-      return s1[i] - s2[i];
+    if ((unsigned char)s1[i] != (unsigned char)s2[i]) {
+      return (unsigned char)s1[i] - (unsigned char)s2[i];
     }
     i++;
   }
-  return s1[i] - s2[i];
+  return (unsigned char)s1[i] - (unsigned char)s2[i];
 }
