ded30885a407b27a0d8f928d7c09669905dcfc3b
Ivan Hrekov <iandrc9@gmail.com>
2025-06-17 21:45:33 +0200

    inspect arguments

---
+++ README.md
@@ -0,0 +1,15 @@
+echo 
+
+Playing with C language. Trying to re-implement `echo` utility.
+
+# Build
+I don't use anything fancy. Just `gcc`:
+```
+gcc echo -o echo
+```
+
+# Test (manual)
+```
+./echo <your-arguments>
+```
+
--- echo.c
+++ echo.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main() {
-  char c;
+int main(int argc, char *argv[]) {
+  printf("argc = %d\n", argc);
 
-  while ((c = getchar()) != EOF) {
-    putchar(c);
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d] = %s\n", i, argv[i]);
   }
 
   return 0;
