a00685d49df0582eee4e59a07de0873488b28051
Ivan Hrekov <iandrc9@gmail.com>
2025-06-18 10:41:11 +0200

    feat: add flag parsing with support for multiple -n
    Introduce a flags struct to handle command-line options in a more
    scalable way. This prepares the code for supporting additional
    options like -e, -E, --help, and --version.
    
    Currently, only the -n flag is supported. Multiple -n flags at the
    start of the argument list are handled, consistent with the behavior
    of /bin/echo.

--- echo.c
+++ echo.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int strcmp(const char *s1, const char *s2) {
+// TODO: I need to implement the following flags:
+// -e
+// -E (default)
+// --help
+// --version
+
+// TODO: warn about unknown flags (or ignore them)
+typedef struct {
+  int no_newline;
+  int first_arg_index;
+} echo_flags;
+
+int my_strcmp(const char *s1, const char *s2) {
   while (*s1 && *s2) {
     if (*s1 != *s2) {
       return (unsigned char)*s1 - (unsigned char)*s2;
@@ -12,12 +24,30 @@ int strcmp(const char *s1, const char *s2) {
   return (unsigned char)*s1 - (unsigned char)*s2;
 }
 
+void parse_flags(int argc, char *argv[], echo_flags *flags) {
+  flags->no_newline = 0;
+  flags->first_arg_index = 1;
+
+  for (int i = 1; i < argc; i++) {
+    const char *arg = argv[i];
+
+    // Handles repeated "-n" like real `echo`
+    if (my_strcmp(arg, "-n") == 0) {
+      flags->no_newline = 1;
+    } else {
+      flags->first_arg_index = i;
+      return;
+    }
+  }
+}
+
 // TODO: add documentation
 // TODO: add quoting, special chars and escape sequence handling (check how echo
 // behaves)
 int main(int argc, char *argv[]) {
-  int no_newline = argc > 1 && strcmp(argv[1], "-n") == 0;
-  int i = no_newline ? 2 : 1;
+  echo_flags flags;
+  parse_flags(argc, argv, &flags);
+  int i = flags.first_arg_index;
 
   while (i < argc) {
     printf("%s", argv[i]);
@@ -26,7 +56,7 @@ int main(int argc, char *argv[]) {
     }
     i++;
   }
-  if (!no_newline) {
+  if (!flags.no_newline) {
     printf("\n");
   }
 
