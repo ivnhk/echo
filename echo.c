c62909cfac18fdce19f8ed12c3b81e8dfe67f266
Ivan Hrekov <iandrc9@gmail.com>
2025-06-23 21:24:32 +0200

    feat: implement -e and -E flags for escape sequences
    Adds support for the `-e` and `-E` flags to the echo clone.
    
    The `-e` flag enables interpretation of escape sequences such as:
      \a \b \c \e \f \n \r \t \v \\ \0NNN \xHH
    
    The `-E` flag disables escape interpretation (default behavior),
    matching GNU coreutils echo.
    
    Unknown flags are treated as arguments for now, though this will be
    addressed in the future.
    
    Escape handling includes:
    - Octal (\0NNN): up to 3 digits
    - Hex (\xHH): up to 2 digits
    - Immediate exit on \c

--- echo.c
+++ echo.c
@@ -1,13 +1,12 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-// TODO: I need to implement the following flags:
-// -e
-// -E (default)
 
-// TODO: warn about unknown flags (or ignore them)
+// TODO: warn about unknown flags (and ignore them)
 typedef struct {
   int no_newline;
   int first_arg_index;
+  int interpret_escapes;
 } echo_flags;
 
 int strcmp_learn(const char *s1, const char *s2) {
@@ -30,7 +29,7 @@ void print_help() {
   printf("-e        enable interpretation of backslash escapes\n");
   printf("-E        disable interpretation of backslash escapes (default)\n");
   printf("--help    display this help and exit\n");
-  printf("--version output version information and exist\n");
+  printf("--version output version information and exit\n");
 }
 
 void print_version() {
@@ -38,9 +37,14 @@ void print_version() {
   printf("written by Ivan Hrekov\n");
 }
 
+// TODO: refactor flag parsing to something a bit more sophisticated
+// 1. Detect if it's a flag. If it is - then parse it, and act accordingly
+// 2. If not, then notify user about it.
+// Q: can there be multiple flags combined like -eEn
 void parse_flags(int argc, char *argv[], echo_flags *flags) {
   flags->no_newline = 0;
   flags->first_arg_index = 1;
+  flags->interpret_escapes = 0; // default -E behavior
 
   for (int i = 1; i < argc; i++) {
     const char *arg = argv[i];
@@ -54,6 +58,10 @@ void parse_flags(int argc, char *argv[], echo_flags *flags) {
     } else if (strcmp_learn(arg, "--version") == 0) {
       print_version();
       exit(0);
+    } else if (strcmp_learn(arg, "-e") == 0) {
+      flags->interpret_escapes = 1;
+    } else if (strcmp_learn(arg, "-E") == 0) {
+      flags->interpret_escapes = 0;
     } else {
       flags->first_arg_index = i;
       return;
@@ -61,15 +69,104 @@ void parse_flags(int argc, char *argv[], echo_flags *flags) {
   }
 }
 
-// TODO: add quoting, special chars and escape sequence handling (check how echo
-// behaves)
+int hex_to_int(char c) {
+  if (c >= '0' && c <= '9')
+    return c - '0';
+  else if (c >= 'a' && c <= 'f')
+    return 10 + (c - 'a');
+  else if (c >= 'A' && c <= 'F')
+    return 10 + (c - 'A');
+  else
+    return -1; // Invalid hex digit
+}
+
+void print_escaped(const char *s) {
+  while (*s) {
+    if (*s == '\\') {
+      s++;
+      if (!*s) {
+        exit(0);
+      }
+      switch (*s) {
+      case 'a':
+        putchar('\a');
+        break;
+      case 'b':
+        putchar('\b');
+        break;
+      case 'c':
+        exit(0); // I see it as a correct behavior
+        break;
+      case 'e':
+        putchar('\x1B');
+        break;
+      case 'f':
+        putchar('\f');
+        break;
+      case 'n':
+        putchar('\n');
+        break;
+      case 'r':
+        putchar('\r');
+        break;
+      case 't':
+        putchar('\t');
+        break;
+      case 'v':
+        putchar('\v');
+        break;
+      case '\\':
+        putchar('\\');
+        break;
+      case '0': {
+        int val = 0, count = 0;
+        s++;
+        while (*s >= '0' && *s <= '7' && count < 3) {
+          val = val * 8 + (*s - '0');
+          s++;
+          count++;
+        }
+        putchar(val);
+        s--; // Q: should it be here?
+      } break;
+      case 'x': {
+        int val = 0, count = 0;
+        s++;
+        while (count < 2 && isxdigit(*s)) {
+          val = val * 16 + hex_to_int(*s);
+          s++;
+          count++;
+        }
+        putchar(val);
+      } break;
+      default:
+        putchar('\\');
+        putchar(*s);
+        break;
+      }
+    } else {
+      putchar(*s);
+    }
+    s++;
+  }
+}
+
+// Q: should I add examples in order to show how to use the thing
 int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    return 0;
+  }
   echo_flags flags;
   parse_flags(argc, argv, &flags);
-  int i = flags.first_arg_index;
 
+  int i = flags.first_arg_index;
   while (i < argc) {
-    printf("%s", argv[i]);
+    const char *arg = argv[i];
+    if (flags.interpret_escapes) {
+      print_escaped(arg);
+    } else {
+      printf("%s", arg);
+    }
     if (i != argc - 1) {
       printf(" ");
     }
